f7f055c64ef581f6ca351d1de399dd8a665a3789
Zubair <zubairsanaullah1122@gmail.com>
2026-04-29 17:06:46 +0500

    pending

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,7 +1,16 @@
-const page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const EventDetailsPage = async({params}: {params: Promise<{ slug: string } >}) => {
+  const {slug} = await params;
+  const response = await fetch(`${BASE_URL}/api/events/${slug}`);
+  const {event} = await response.json();
+
   return (
-    <div>page</div>
+    <section>
+      <h1>Event Details: <br /> {event.title}</h1>
+      
+    </section>
   )
 }
 
-export default page
+export default EventDetailsPage
